db82c881954de6fc32edf171d9aff22ba14bc97d
Harsha vardhan <120001431+smtart@users.noreply.github.com>
2023-01-26 18:43:57 +0530

    Update utils.py

--- helper/utils.py
+++ helper/utils.py
@@ -63,18 +63,20 @@ def generate_response(message: str) -> str:
         - str: formated response for the command
     '''
     if message == '/contactme':
-        return 'You can reach out to me here: https://instagram.com/k_harshavardhan_?igshid=ZDdkNTZiNTM='
-    elif message == '/Imagechatbot': 
-        return 'It convert your descriptions into images'
+        return 'You can reach out to me here: https://rajkkapadia-portfolio.onrender.com/'
     elif message == '/youtube':
-        return 'You can watch my video tutorials here: https://youtube.com/@Harshavaardhann'
+        return 'You can watch my video tutorials here: https://www.youtube.com/channel/UCOT01XvBSj12xQsANtTeAcQ'
+    elif message == '/github':
+        return 'You can get helpful piece of code here: https://github.com/RajKKapadia'
     elif message == '/buyacoffee':
-        return 'If you like my work please consider buying me a coffee here: https://www.buymeacoffee.com/Harshavardhann '
+        return 'If you like my work please consider buying me a coffee here: https://www.buymeacoffee.com/rajkkapadia'
+    elif message == '/help':
+        return 'You can ask almost anything here, but do not belive whatever this bot says. :-)'
     elif message == '/start':
-        return 'Hello! Im a chatbot created by Harshavardhan. How can I help you today?'
-   else:
-         result = text_complition(message)
-         if result['status'] == 1:
-             return result['response'].strip()
-         else:
+        return 'Hi, this is a chat-bot that uses OpenAI GPT-3, developed by me with love. I will not spam you for sure.'
+    else:
+        result = text_complition(message)
+        if result['status'] == 1:
+            return result['response'].strip()
+        else:
             return 'Sorry, I am out of service at this moment.'
